670b195e4d6268f86aabd2b1b0a6b5116e7c1142
Dinh Hieu <dinhhieu3072004@gmail.com>
2026-01-15 10:41:43 +0700

    Fix hardcoded

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'my_secret_key';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
